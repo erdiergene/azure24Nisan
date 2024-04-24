cbabdbafba576f6caf7b35582d1d391e44671b4c
erdi ergene <erdieergene@gmail.com>
2024-04-24 20:27:14 +0300

    hello first commit

---
+++ Azure24Nisan/Azure24Nisan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Azure24Nisan
+//
+//  Created by Erdi Ergene on 24.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
